e073eea648391eeec732ac644e9b3676cb4ff052
Kirill Yakovenko <me.kyakovenko@ya.ru>
2021-10-21 23:17:44 +0300

    Add lab2

---
+++ lab2/BundleOfTen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BundleOfTen
+{
+    namespace ht4
+    {
+        class LastNumber
+        {
+            static int ReadNum()
+            {
+                int res = 0;
+                bool done = false;
+                while (!done)
+                {
+                    try
+                    {
+                        res = int.Parse(Console.ReadLine());
+                        done = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error, try again");
+                    }
+                }
+                return res;
+            }
+            static void Main(string[] args)
+            {
+                int n = 0;
+                int k = 0;
+                Console.WriteLine("Enter a random num!");
+                do
+                {
+                    Console.WriteLine("Num must be under 10000!");
+                    n = ReadNum();
+                } while (n > 10000);
+                n = n / 10;
+                k = n % 10;
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
---
+++ lab2/LastNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LastNumber
+{
+    namespace ht3
+    {
+        class LastNumber
+        {
+            static int ReadNum()
+            {
+                int res = 0;
+                bool done = false;
+                while (!done)
+                {
+                    try
+                    {
+                        res = int.Parse(Console.ReadLine());
+                        done = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error, try again");
+                    }
+                }
+                return res;
+            }
+            static void Main(string[] args)
+            {
+                int n = 0;
+                int k = 0;
+                Console.WriteLine("Enter a random num!");
+                do
+                {
+                    Console.WriteLine("num must be under 10000");
+                    n = ReadNum();
+                } while (n > 10000);
+                k = n % 10;
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
